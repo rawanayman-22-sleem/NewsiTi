41773c1ff1e51f8f6ca8ff9e499a5fe5729b4081
Rawan <rawanaymansleem@gmail.com>
2023-09-23 00:06:23 +0300

    r

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:itiproject/screens/DetailsScreen.dart';
 import 'package:itiproject/screens/HomeScreen.dart';
+import 'package:itiproject/screens/SplashScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      home: SplashScreen(),
     );
   }
 }
---
+++ lib/screens/SplashScreen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:itiproject/screens/HomeScreen.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const HomeScreen()));
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/image/NwesSplash.jpg',),
+            // CircularProgressIndicator(
+            //   color:Colors.black ,
+            // )
+          ],
+        ),
+      ),
+
+    );
+  }
+}
